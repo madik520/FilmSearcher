cc71e52e2ae007f08398e8c9f1c998c4f96255ec
madik520 <44100702+madik520@users.noreply.github.com>
2019-07-19 14:41:55 +0300

    Updates

---
+++ precache-manifest.316d810873e53595c824ad46b2cb53f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041dd2323abde09f5c4597d601f83b1c",
+    "url": "/FilmSearcher/index.html"
+  },
+  {
+    "revision": "1cabfa654238208d86d0",
+    "url": "/FilmSearcher/static/css/2.32bbf376.chunk.css"
+  },
+  {
+    "revision": "c81779de603c324deb4a",
+    "url": "/FilmSearcher/static/css/main.3e3d431c.chunk.css"
+  },
+  {
+    "revision": "1cabfa654238208d86d0",
+    "url": "/FilmSearcher/static/js/2.d0fecf8b.chunk.js"
+  },
+  {
+    "revision": "c81779de603c324deb4a",
+    "url": "/FilmSearcher/static/js/main.3e23eb9c.chunk.js"
+  },
+  {
+    "revision": "642180dd30feb0601a86",
+    "url": "/FilmSearcher/static/js/runtime~main.5dea5ac1.js"
+  },
+  {
+    "revision": "208d4fa516abfebf2179834e4cefd244",
+    "url": "/FilmSearcher/static/media/bg.208d4fa5.jpg"
+  }
+]);
